a9423e9078ffcec2278f972514b329ebb6f465ed
wing chan <wingchan@gmstoneft.com>
2020-03-25 18:17:03 +0800

    catch news_list from official website, and get void_battles_table, high dragon master table

--- my_app/views.py
+++ my_app/views.py
@@ -1,10 +1,13 @@
 import requests
+import pandas as pd
 from django.shortcuts import render
 from bs4 import BeautifulSoup
 from requests.compat import quote_plus
+from selenium import webdriver
 # Create your views here.
 
 BASE_URL = 'https://dragalialost.gamepedia.com{}'
+NEWS_URL = 'https://dragalialost.com{}'
 
 
 def home(request):
@@ -16,14 +19,55 @@ def home(request):
     response = requests.get(BASE_URL.format('/Dragalia_Lost_Wiki'))
     soup = BeautifulSoup(response.text, features='html.parser')
 
-    new_listings = soup.find_all('div', class_="SecondaryEventsMP")
-    for new_list in new_listings:
-        new_listing_image = new_list.find('img').get('src')
-        new_list_url = new_list.find_all('a')[0:1][0].get('href')
-        new_list_title = new_list.find_all('a')[0:1][0].get('title')
-        official_new_list_url = new_list.find_all('a')[2:3][0].get('href')
-        news_response_lists.append(
-            (new_listing_image, BASE_URL.format(new_list_url), new_list_title, official_new_list_url))
+    # table = soup.find('table', class_='wikitable center').tbody
+    # rows = table.find_all('tr')
+    # columns = [v.text.replace('\n', '') for v in rows[0].find_all('th')]
+    # df = pd.DataFrame(columns=columns)
+    # for i in range(1, len(rows)):
+    #     tds = rows[i].find_all('td')
+    #     values = [td.text.replace('\n', '') for td in tds]
+    #     df = df.append(pd.Series(values, index=columns), ignore_index=True)
+
+    void_battles_table = soup.find_all('table', class_='wikitable center')[0]
+    void_battles_table_body = []
+    void_battles_table_head = [
+        th.text for th in void_battles_table.find_all('th')]
+    for row in void_battles_table.find_all('tr'):
+        void_battles_table_body.append(
+            [cell.get_text(strip=True) for cell in row.find_all('td')])
+
+    high_dragon_master_table = soup.find_all(
+        'table', class_='wikitable center')[1]
+    high_dragon_master_body = []
+    high_dragon_master_head = [
+        th.text for th in high_dragon_master_table.find_all('th')]
+    for row in high_dragon_master_table.find_all('tr'):
+        high_dragon_master_body.append(
+            [cell.get_text(strip=True) for cell in row.find_all('td')])
+
+    # for render news
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # driver = webdriver.Chrome(chrome_options=options)
+    # driver.get(NEWS_URL.format('/cht'))
+    # html = driver.execute_script("return document.documentElement.outerHTML")
+    # sel_soup = BeautifulSoup(html, features='html.parser')
+
+    # new_list = sel_soup.find_all('ul', class_='news-list')
+    # for ul_tag in new_list:
+    #     for li_tag in ul_tag.find_all('li'):
+    #         new_list_url = NEWS_URL.format(li_tag.find('a').get('href'))
+    #         if "http" in li_tag.find('img').get('src'):
+    #             new_list_image = li_tag.find('img').get('src')
+    #         else:
+    #             new_list_image = NEWS_URL.format(li_tag.find('img').get('src'))
+    #         new_list_title = li_tag.find(
+    #             'p', class_='title').get_text(strip=True)
+    #         new_list_time = li_tag.find(
+    #             'div', class_='time').get_text(strip=True)[0:16]
+    #         new_list_cat = li_tag.find('span').get_text(strip=True)
+    #         news_response_lists.append(
+    #             (new_list_image, new_list_url, new_list_title, new_list_time, new_list_cat))
 
     # for render showcase
     showcase_lists = soup.find_all('div', class_="SummonShowcaseMP")
@@ -77,5 +121,10 @@ def home(request):
         'new_adventurers_response_lists': new_adventurers_response_lists,
         'new_dragons_response_lists': new_dragons_response_lists,
         'new_wyrmprints_response_lists': new_wyrmprints_response_lists,
+        'void_battles_table_head': void_battles_table_head,
+        'void_battles_table_body': void_battles_table_body,
+        'high_dragon_master_head': high_dragon_master_head,
+        'high_dragon_master_body': high_dragon_master_body,
+
     }
     return render(request, 'index.html', stuff_for_frontend)
