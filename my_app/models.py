0552da2feb1c674945ce6bb59877da9ff72f5d82
wing chan <wingchan@gmstoneft.com>
2020-04-02 18:29:41 +0800

    add Adventurer models

--- my_app/models.py
+++ my_app/models.py
@@ -1,3 +1,57 @@
-from django.db import models
+from djongo import models
 
 # Create your models here.
+
+
+def upload_location(instance, filename):
+    return "%s/%s" % (instance, filename)
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=50)
+    icon = models.ImageField(upload_to=upload_location, blank=True, null=True,
+                             width_field="width_field", height_field="height_field")
+    level_1 = models.CharField(max_length=255)
+    level_2 = models.CharField(max_length=255)
+    level_3 = models.CharField(max_length=255)
+
+
+class Adventurer(models.Model):
+    name = models.CharField(unique=True, max_length=255)
+    classname = models.CharField(max_length=10)
+    element = models.CharField(max_length=10)
+    weapon = models.CharField(max_length=15)
+    rarity = models.CharField(max_length=1)
+    availability = models.CharField(max_length=20)
+    hp = models.CharField(max_length=5)
+    Str = models.CharField(max_length=5)
+    total_max_hp = models.CharField(max_length=5, blank=True, null=True)
+    total_max_str = models.CharField(max_length=5, blank=True, null=True)
+    base_min_might = models.CharField(max_length=6, blank=True, null=True)
+    base_max_might = models.CharField(max_length=6, blank=True, null=True)
+    defense = models.CharField(max_length=5, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    race = models.CharField(max_length=15, blank=True, null=True)
+    obtained_form = models.CharField(max_length=50, blank=True, null=True)
+    icon = models.ImageField(upload_to=upload_location, blank=True, null=True,
+                             width_field="width_field", height_field="height_field")
+    image = models.ImageField(upload_to=upload_location, blank=True, null=True,
+                              width_field="width_field", height_field="height_field")
+    url = models.URLField()
+    skill_1 = models.ArrayField(model_container=Skill,)
+    skill_2 = models.ArrayField(model_container=Skill,)
+    remark = models.CharField(max_length=255, blank=True, null=True)
+    remark2 = models.CharField(max_length=255, blank=True, null=True)
+    remark3 = models.CharField(max_length=255, blank=True, null=True)
+    remark4 = models.CharField(max_length=255, blank=True, null=True)
+    remark5 = models.CharField(max_length=255, blank=True, null=True)
+    release_date = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.name:
+            super().save(*args, **kwargs)
--- my_app/tests.py
+++ my_app/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+from django.test.client import Client
+from bs4 import BeautifulSoup
+from .models import Adventurer, Skill
+import requests
 
-# Create your tests here.
+try:
+    basestring
+except NameError:
+    basestring = str
+
+
+# class MyClient(Client):
+#     def request(self, **request):
+#         response = super(MyClient, self).request(**request)
+#         try:
+#             response.soup = BeautifulSoup(response.content, "html.parser")
+#         except:
+#             response.soup = None
+#         return response
+
+
+class AdventurerModelTests(TestCase):
+    # client_class = MyClient
+
+    def test_import_data_adventurer(self):
+        response = requests.get(
+            'https://dragalialost.gamepedia.com/Dragalia_Lost_Wiki')
+        new_adventurer = Adventurer(name='hello')
+        self.assertEqual(new_adventurer.name, 'faliks')
+        self.assertEqual(response.status_code, 200)
